468f94ce8344adf8d416059c64e200b019f7e2ca
Park Changyong <me@changyong.me>
2026-04-05 23:23:47 +0900

    Move fnm initialization from conf.d into config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 /opt/homebrew/bin/brew shellenv fish | source
 source ~/.orbstack/shell/init2.fish 2>/dev/null; or true
+fnm env --use-on-cd --shell fish | source
 
 fish_add_path /opt/homebrew/opt/rustup/bin
 fish_add_path ~/.cargo/bin
--- fish/setup.sh
+++ fish/setup.sh
@@ -1,3 +1,2 @@
-ln -snf ~/.dotfiles/fish/conf.d/fnm.fish ~/.config/fish/conf.d/fnm.fish
 ln -snf ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -snf ~/.dotfiles/fish/starship.toml ~/.config/starship.toml
